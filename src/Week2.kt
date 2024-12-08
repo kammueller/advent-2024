77f510a27f36ddfde1a0a865567ab56d85e5448d
Matthias Kammüller <matthias.kammueller@rio.cloud>
2024-12-08 14:09:00 +0100

    [08-02] solved

--- src/Week2.kt
+++ src/Week2.kt
@@ -10,6 +10,13 @@ fun december08puzzle() {
     fun Coordinates.isValid(): Boolean =
         first in (0 until length) && second in (0 until width)
 
+    operator fun Coordinates.plus(other: Coordinates): Coordinates =
+        Pair(first + other.first, second + other.second)
+
+    operator fun Coordinates.minus(other: Coordinates): Coordinates =
+        Pair(first - other.first, second - other.second)
+
+
     fun findAllSenders(f: Char): List<Coordinates> {
         val result = mutableListOf<Coordinates>()
         for (i in 0 until length) {
@@ -25,37 +32,63 @@ fun december08puzzle() {
     fun <T> getCombinations(list: List<T>): List<Pair<T, T>> {
         val result = mutableListOf<Pair<T, T>>()
         list.indices.forEach { i ->
-            (i+1 until list.size).forEach{ j ->
+            (i + 1 until list.size).forEach { j ->
                 result += Pair(list[i], list[j])
             }
         }
         return result
     }
 
-    fun getAntiNodesForStations(a: Coordinates, b:Coordinates): List<Coordinates> {
-        val (x1, y1) = a
-        val (x2, y2) = b
-        val diffX = x2-x1
-        val diffY = y2-y1
+    fun getAntiNodesForStations(a: Coordinates, b: Coordinates): List<Coordinates> {
+        val diff = b - a
+        val antiNodes = listOf(a - diff, b + diff)
 
-        val antiNodes = listOf(Pair(x1-diffX, y1-diffY), Pair(x2+diffX, y2+diffY))
+        return antiNodes.filter { it.isValid() }
+    }
 
-        return antiNodes.filter {it.isValid()}
+    fun getHarmonicAntiNodesForStations(a: Coordinates, b: Coordinates): List<Coordinates> {
+        val diff = b - a
+        var harmonicAntiNodes = mutableListOf<Coordinates>()
+        var candidate = a
+        // go up
+        while (candidate.isValid()) {
+            harmonicAntiNodes.add(candidate)
+            candidate = candidate - diff
+        }
+        // go down
+        candidate = a + diff
+        while (candidate.isValid()) {
+            harmonicAntiNodes.add(candidate)
+            candidate = candidate + diff
+        }
+
+        return harmonicAntiNodes
     }
 
     val antiNodes = Array(length) { Array(width) { false } }
+    val harmonicsAntiNodes = Array(length) { Array(width) { false } }
     for (f in frequencies) {
         val senders = findAllSenders(f)
         getCombinations(senders).forEach { (sender1, sender2) ->
             val newAntiNodes = getAntiNodesForStations(sender1, sender2)
-            newAntiNodes.forEach{ (i,j) -> antiNodes[i][j] = true}
+            newAntiNodes.forEach { (i, j) -> antiNodes[i][j] = true }
+
+            getHarmonicAntiNodesForStations(sender1, sender2).forEach { (i, j) ->
+                harmonicsAntiNodes[i][j] = true
+            }
         }
     }
 
     if (debug) {
         for (i in 0 until length) {
             for (j in 0 until width) {
-               print(if(antiNodes[i][j]) '#' else '.' )
+                if (antiNodes[i][j]) {
+                    print('#')
+                } else if (harmonicsAntiNodes[i][j]) {
+                    print('@')
+                } else {
+                    print('.')
+                }
             }
             println()
         }
@@ -63,4 +96,5 @@ fun december08puzzle() {
     }
 
     println(antiNodes.sumOf { row -> row.count { it } })
+    println(harmonicsAntiNodes.sumOf { row -> row.count { it } })
 }
